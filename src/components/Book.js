1c68caa9637e3d7cdd7968cff3202da1e0d20722
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-06-17 15:37:25 +0330

    Add author and remove button

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,20 @@
-const Book = () => (
+import PropTypes from 'prop-types';
+
+const Book = ({ title, author }) => (
   <div className="book-container">
-    <span className="book-title">Book title</span>
-    <img src="https://img.icons8.com/ios/20/trash--v1.png" alt="trash--v1" />
+    <span className="book-title">{title}</span>
+    <span className="book-author">{author}</span>
+    <button type="submit">Remove</button>
   </div>
 );
+
+Book.propTypes = {
+  obj: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  }).isRequired,
+  // title: PropTypes.object.isRequired,
+  // author: PropTypes.object.isRequired,
+};
 
 export default Book;
--- src/components/Books.js
+++ src/components/Books.js
@@ -2,9 +2,9 @@ import Book from './Book';
 
 const Books = () => (
   <>
-    <Book />
-    <Book />
-    <Book />
+    <Book title="title1" author="author1" />
+    <Book title="title2" author="author2" />
+    <Book title="title3" author="author3" />
   </>
 );
 
