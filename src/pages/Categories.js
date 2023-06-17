0d356dfd7bf7efa2602c1072963e23ed9f2664d9
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-06-17 14:12:27 +0330

    Build categories and home pages

---
+++ src/pages/Categories.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+
+const Categories = () => (
+  <div className="categories-page-container">
+    <Navbar />
+    Categories
+  </div>
+);
+
+export default Categories;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+import Form from '../components/Form';
+import Books from '../components/Books';
+
+const Home = () => (
+  <div className="home-page-container">
+    <Navbar />
+    <Form />
+    <Books />
+  </div>
+);
+
+export default Home;
