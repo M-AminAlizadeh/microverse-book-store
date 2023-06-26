f8486342bcabd1fc7aaa95f43f7b99ddc8160720
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-06-26 19:08:39 +0330

    Build store & add 2 reducer

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { booksSlice } from "./books/booksSlice";
+import { categoriesSlice } from "./categories/categoriesSlice";
+
+export const store = configureStore({
+  reducer: {
+    books: booksSlice,
+    categories: categoriesSlice,
+  },
+});
