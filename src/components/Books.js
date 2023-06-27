2791611faa6eeb46f43c4777d9bbe7206e69b87f
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-06-27 15:20:42 +0330

    Install gh pages

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Book from './Book';
 
 const Books = () => (
-  <>
+  <div>
     <Book title="title1" author="author1" />
     <Book title="title2" author="author2" />
     <Book title="title3" author="author3" />
-  </>
+  </div>
 );
 
 export default Books;
