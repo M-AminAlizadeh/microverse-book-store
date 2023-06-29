64d81283fc6061ba2bbf8e5333728d7b559cf165
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-06-29 12:10:57 +0330

    Use dispatch to add new item

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { add } from "../redux/books/booksSlice";
 
-const Form = () => (
-  <div className="form-container">
-    <h2>Add new book</h2>
-    <form>
-      <input type="text" placeholder="Book title" />
-      <select>
-        <option value="author">Author</option>
-      </select>
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
+const Form = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const dispatch = useDispatch();
+  const handleClickAddBtn = (e) => {
+    e.preventDefault();
+    dispatch(add({ title, author }));
+    // reset form
+    setTitle("");
+    setAuthor("");
+  };
+  return (
+    <div className='form-container'>
+      <h2>Add new book</h2>
+      <form>
+        <input
+          type='text'
+          placeholder='Book title'
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <input
+          type='text'
+          placeholder='Author'
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <button type='submit' onClick={(e) => handleClickAddBtn(e)}>
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,22 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: [],
+  value: [
+    {
+      item_id: "item1",
+      title: "The Great Gatsby",
+      author: "John Smith",
+      category: "Fiction",
+    },
+    {
+      item_id: "item2",
+      title: "Anna Karenina",
+      author: "Leo Tolstoy",
+      category: "Fiction",
+    },
+    {
+      item_id: "item3",
+      title: "The Selfish Gene",
+      author: "Richard Dawkins",
+      category: "Nonfiction",
+    },
+  ],
 };
 
 const booksSlice = createSlice({
-  name: 'books',
+  name: "books",
   initialState,
   reducers: {
     add: (state, action) => {
-      const { title } = action.payload;
-      state.value.push({ title });
+      const { title, author } = action.payload;
+      state.value.push({
+        item_id: `item${Math.floor(Math.random() * 10000)}`,
+        title,
+        author,
+      });
     },
     remove: (state, action) => {
       const { id } = action.payload;
-      state.value = state.value.filter((book) => book.id !== id);
+      state.value = state.value.filter((book) => book.item_id !== id);
     },
   },
 });
 export const { add, remove } = booksSlice.actions;
-export default booksSlice;
+export default booksSlice.reducer;
