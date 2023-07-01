6da38bfde55caf3400e6eb930a004afb63af09e3
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-07-01 12:25:50 +0330

    Get books and display them

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Book from './Book';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Book from "./Book";
+import { fetchBooks } from "../redux/books/booksSlice";
 
 const Books = () => {
-  const books = useSelector((state) => state.books.value);
+  const books = useSelector((state) => state.books.books);
+  const loading = useSelector((state) => state.books.loading);
+  const dispatch = useDispatch();
 
-  return (
-    <div>
-      {books.map((book) => (
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  const bookComponents = [];
+  for (const itemId in books) {
+    const bookArray = books[itemId];
+
+    for (const book of bookArray) {
+      bookComponents.push(
         <Book
           key={book.item_id}
           id={book.item_id}
@@ -15,9 +29,10 @@ const Books = () => {
           author={book.author}
           category={book.category}
         />
-      ))}
-    </div>
-  );
+      );
+    }
+  }
+  return <div>{bookComponents}</div>;
 };
 
 export default Books;
