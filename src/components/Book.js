f145d2ab5781de0c95541a101db8e63ce67947fa
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-07-01 16:46:03 +0330

    Fix eslint issue

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,23 +1,24 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { useDispatch } from "react-redux";
-import { removeBook, fetchBooks } from "../redux/books/booksSlice";
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook, fetchBooks } from '../redux/books/booksSlice';
 
-const Book = ({ title, author, category, id }) => {
+const Book = ({
+  title, author, category, id,
+}) => {
   const dispatch = useDispatch();
 
   const handleRemove = () => {
-    console.log({ itemId: id });
     dispatch(removeBook({ itemId: id }));
     dispatch(fetchBooks());
   };
 
   return (
-    <div className='book-container'>
-      <span className='book-title'>{title}</span>
-      <span className='book-author'>{author}</span>
-      <span className='book-category'>{category}</span>
-      <button type='button' onClick={handleRemove}>
+    <div className="book-container">
+      <span className="book-title">{title}</span>
+      <span className="book-author">{author}</span>
+      <span className="book-category">{category}</span>
+      <button type="button" onClick={handleRemove}>
         Remove
       </button>
     </div>
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import Book from "./Book";
-import { fetchBooks } from "../redux/books/booksSlice";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Book from './Book';
+import { fetchBooks } from '../redux/books/booksSlice';
 
 const Books = () => {
   const books = useSelector((state) => state.books.books);
@@ -16,20 +16,17 @@ const Books = () => {
     return <div>Loading...</div>;
   }
 
-  console.log(books);
   return (
     <div>
-      {Object.entries(books).map(([key, bookItems]) =>
-        bookItems.map((book) => (
-          <Book
-            key={key}
-            id={key}
-            title={book.title}
-            author={book.author}
-            category={book.category}
-          />
-        ))
-      )}
+      {Object.entries(books).map(([key, bookItems]) => bookItems.map((book) => (
+        <Book
+          key={key}
+          id={key}
+          title={book.title}
+          author={book.author}
+          category={book.category}
+        />
+      )))}
     </div>
   );
 };
