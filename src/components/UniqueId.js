e2982522b2b68eb765c909989686238844ecf053
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-07-01 13:55:53 +0330

    Delete extra comments

--- src/components/UniqueId.js
+++ src/components/UniqueId.js
@@ -3,20 +3,17 @@ import axios from 'axios';
 const UniqueId = () => {
   let AppID = '';
   if (!localStorage.getItem('AppID')) {
-    // axios
     axios
       .post(
         'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/',
       )
       .then((response) => {
         AppID = response.data;
-        // set to localstoreage for next times
         localStorage.setItem('AppID', JSON.stringify(AppID));
       });
   } else {
     AppID = localStorage.getItem('AppID');
   }
-  // return AppID;
 };
 
 export default UniqueId;
